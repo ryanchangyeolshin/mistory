4fb3e0719440b1c84c9ab24774a78972a0926bfe
Ryan Shin <changyeolshin@gmail.com>
2017-11-06 10:40:15 -0800

    Created a GET /api/users/ route where all of the users' information can be accessed. #1

--- server/routers/users-router.js
+++ server/routers/users-router.js
@@ -4,10 +4,20 @@ const usersRouter = (users) => {
   const router = new Router()
 
   router
-    .post('/', async (req, res) => {
+    .post('/', async ({ body }, res) => {
       try {
-        const user = await users.insertOne(req.body)
-        res.status(200).json(user)
+        const user = await users.insertOne(body)
+        res.status(201).json(user)
+      }
+      catch (err) {
+        console.error(err)
+        res.sendStatus(400)
+      }
+    })
+    .get('/', async (req, res) => {
+      try {
+        const usersData = await users.find().toArray()
+        res.status(202).json(usersData)
       }
       catch (err) {
         console.error(err)
