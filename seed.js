880502869f8f440169338a3789a54e292d48ab70
Ryan Shin <changyeolshin@gmail.com>
2017-11-06 15:48:06 -0800

    Added the seed.js file containing 3 fake stories. #2

---
+++ seed.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb')
+
+const storiesData = [
+  {
+    title: 'I saw a ghost!',
+    author: 'Ryan Shin',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'I almost won a match in Rocket League.',
+    author: 'Peter Hermansen',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'I almost won the lottery!',
+    author: 'Riley Rangel',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
+
+MongoClient.connect(process.env.MONGODB_URI, async (err, db) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  const stories = db.collection('stories')
+  await stories.insertMany(storiesData)
+
+})
