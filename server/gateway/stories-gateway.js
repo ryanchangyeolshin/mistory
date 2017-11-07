ef540d3e8f54177a825f722fae6b6edb4b9bfe31
Ryan Shin <changyeolshin@gmail.com>
2017-11-07 13:36:54 -0800

    Fixed the return output of stories-gateway.js. #2

--- server/gateway/stories-gateway.js
+++ server/gateway/stories-gateway.js
@@ -1,8 +1,8 @@
 module.exports = stories => {
   return {
     async findAllStories() {
-      const { ops } = await stories.find().toArray()
-      return ops
+      const storiesData = await stories.find().toArray()
+      return storiesData
     }
   }
 }
