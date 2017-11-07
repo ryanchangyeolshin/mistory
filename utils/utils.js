2effcf7fbd46ae06d9223f5e1dd07fed5cecb455
Ryan Shin <changyeolshin@gmail.com>
2017-11-06 16:16:12 -0800

    Fixed the omit function. #1

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,9 @@
 const omit = (target, keys) => {
   return Object.keys(target)
-    .filter(key => !key.includes(key))
-    .reduce((copy, key) => {
+    .filter(key => !keys.includes(key))
+    .reduce((copy, key) =>
       Object.assign(copy, { [key]: target[key] })
-    })
+      , {})
 }
 
 module.exports = omit
