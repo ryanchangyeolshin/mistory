eb7806c5c51b1ccac72b8da8abd8f2712f849e20
Ryan Shin <changyeolshin@gmail.com>
2017-11-06 16:20:57 -0800

    Wrapped the TextFields inside a div for block-level elements. #1

--- client/users-form.js
+++ client/users-form.js
@@ -11,41 +11,51 @@ export default function UsersForm({ style, handleSubmit }) {
     <Paper style={userFormStyle} zDepth={1}>
       <h1>Create an Account!</h1>
       <form onSubmit={handleSubmit}>
-        <TextField
-          hintText="Please type a username."
-          floatingLabelText="Username"
-          type="text"
-          name="username"
-          fullWidth = {true}
-        /><br />
-        <TextField
-          hintText="Please type a password."
-          floatingLabelText="Password"
-          type="password"
-          name="password"
-          fullWidth = {true}
-        /><br />
-        <DatePicker
-          hintText="Please provide your birthdate."
-          openToYearSelection={true}
-          fullWidth = {true}
-          name="birthdate"
-          style={{ marginTop: '10px' }}
-        />
-        <TextField
-          hintText="Please type your email."
-          floatingLabelText="Email Address"
-          type="email"
-          name="email"
-          fullWidth = {true}
-        /><br />
-        <TextField
-          hintText="Please confirm your email address."
-          floatingLabelText="Confirm Email Address"
-          type="email"
-          name="confirmEmail"
-          fullWidth = {true}
-        /><br />
+        <div>
+          <TextField
+            hintText="Please type a username."
+            floatingLabelText="Username"
+            type="text"
+            name="username"
+            fullWidth = {true}
+          />
+        </div>
+        <div>
+          <TextField
+            hintText="Please type a password."
+            floatingLabelText="Password"
+            type="password"
+            name="password"
+            fullWidth = {true}
+          />
+        </div>
+        <div>
+          <DatePicker
+            hintText="Please provide your birthdate."
+            openToYearSelection={true}
+            fullWidth = {true}
+            name="birthdate"
+            style={{ marginTop: '10px' }}
+          />
+        </div>
+        <div>
+          <TextField
+            hintText="Please type your email."
+            floatingLabelText="Email Address"
+            type="email"
+            name="email"
+            fullWidth = {true}
+          />
+        </div>
+        <div>
+          <TextField
+            hintText="Please confirm your email address."
+            floatingLabelText="Confirm Email Address"
+            type="email"
+            name="confirmEmail"
+            fullWidth = {true}
+          />
+        </div>
         <RaisedButton type="submit" label="Submit" primary={true} style={{ margin: '20px 0' }} />
       </form>
     </Paper>
