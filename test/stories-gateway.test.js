19359d74984972810320891852085b2f6057e0ac
Ryan Shin <changyeolshin@gmail.com>
2017-11-09 14:47:17 -0800

    Provided unit testing for the createStory() function inside of the stories-gateway.js file. #3

--- test/stories-gateway.test.js
+++ test/stories-gateway.test.js
@@ -10,6 +10,7 @@ describe('storiesGateway', () => {
   let stories
   let story
   let findAllStories
+  let createStory
 
   before('Connect to MongoDB', done => {
     MongoClient.connect(process.env.MONGODB_URI, (err, _db) => {
@@ -26,6 +27,7 @@ describe('storiesGateway', () => {
         image: 'https://i.pinimg.com/originals/4e/b5/5b/4eb55b8b5de9bd8ae4ecec61b5fb2f55.jpg'
       }
       findAllStories = storiesGateway(stories).findAllStories
+      createStory = storiesGateway(stories).createStory
       done()
     })
   })
@@ -48,6 +50,24 @@ describe('storiesGateway', () => {
       expect(title).to.equal(story.title)
       expect(content).to.equal(story.content)
       expect(image).to.equal(story.image)
+    })
+  })
+
+  describe('createStory()', () => {
+    it('should insert a new story and return it', async () => {
+      const testStory = {
+        id: uuid(),
+        title: 'I aced the exam!',
+        author: 'Johnny Woo',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        image: 'http://blog.homefinder.com/wp-content/uploads/2013/11/aplus4.jpg'
+      }
+      const { id, title, author, content, image } = await createStory(testStory)
+      expect(id).to.equal(testStory.id)
+      expect(title).to.equal(testStory.title)
+      expect(author).to.equal(testStory.author)
+      expect(content).to.equal(testStory.content)
+      expect(image).to.equal(testStory.image)
     })
   })
 })
