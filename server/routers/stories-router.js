f2758459362313419120133864734030881b5578
Ryan Shin <changyeolshin@gmail.com>
2017-11-09 14:35:45 -0800

    Created a POST /api/stories/ route inside of the stories-router.js file. #3

--- server/routers/stories-router.js
+++ server/routers/stories-router.js
@@ -1,14 +1,28 @@
 const { Router } = require('express')
 const storiesGateway = require('../gateway/stories-gateway')
+const multerUpload = require('../multer')
+const googleGateway = require('../google-gateway')
 
 const storiesRouter = stories => {
-  const { findAllStories } = storiesGateway(stories)
+  const { findAllStories, createStory } = storiesGateway(stories)
   const router = new Router()
 
   router
     .get('/', async (req, res) => {
       const stories = await findAllStories()
       res.status(200).json(stories)
+    })
+    .post('/', multerUpload, async ({ jpeg, mp3, body: { title, content } }, res) => {
+      const data = {
+        title: title,
+        content: title,
+        image: jpeg.fileName,
+        audio: mp3.fileName
+      }
+      const story = createStory(data)
+      googleGateway(jpeg.fileName).upload()
+      googleGateway(mp3.fileName).upload()
+      res.status(201).json(story)
     })
 
   return router
