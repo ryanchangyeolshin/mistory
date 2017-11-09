675e42f797c28622b007defd5845c97ed299d693
Ryan Shin <changyeolshin@gmail.com>
2017-11-09 10:55:39 -0800

    Created the google-gateway.js file to return a storage upload function. #3

---
+++ server/google-gateway.js
@@ -0,0 +1,20 @@
+require('dotenv/config')
+const fs = require('fs')
+const Storage = require('@google-cloud/storage')
+
+const storage = new Storage()
+
+module.exports = function googleGateway(file) {
+  return {
+    upload() {
+      storage
+        .bucket(process.env.BUCKET)
+        .upload('server/uploads' + file)
+        .then(() => {
+          console.log(`${file} uploaded to ${process.env.BUCKET}.`)
+          return fs.unlink('server/uploads' + file)
+        })
+        .catch(err => console.error('ERROR:', err))
+    }
+  }
+}
