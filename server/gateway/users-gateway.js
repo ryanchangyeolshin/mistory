b0acf3c396ccee3667678e098e3aa1044dc3ebec
Ryan Shin <changyeolshin@gmail.com>
2017-11-17 11:08:33 -0800

    Added a gateway function called findUser() inside the users-gateway.js file. #9

--- server/gateway/users-gateway.js
+++ server/gateway/users-gateway.js
@@ -6,6 +6,10 @@ module.exports = users => {
       const { ops } = await users.insertOne(userInfo)
       const omitted = omit(ops[0], ['password'])
       return omitted
+    },
+    async findUser(username) {
+      const user = await users.findOne({ username })
+      return user
     }
   }
 }
