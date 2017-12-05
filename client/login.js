00df300cf7d50a2a72261d15016c7082ee937e6d
Ryan Shin <changyeolshin@gmail.com>
2017-12-04 23:26:41 -0800

    Refactored the Login component. #9

--- client/login.js
+++ client/login.js
@@ -3,35 +3,38 @@ import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
 import Typography from 'material-ui/Typography'
 import Button from 'material-ui/Button'
+import Validation from './HOC/validation'
 import styles from './styles/styles'
 
-export default function Login({ handleUserLogin }) {
+export default function Login({ username, handleUserLogin }) {
   return (
-    <Paper style={styles.loginForm}>
-      <Typography type="headline" component="h1">Login!</Typography>
-      <form onSubmit={handleUserLogin}>
-        <div>
-          <TextField
-            type="text"
-            label="Username"
-            name="username"
-            style={styles.loginInputField}
-            fullWidth={true}
-          />
-        </div>
-        <div>
-          <TextField
-            type="text"
-            label="Password"
-            name="password"
-            style={styles.loginInputField}
-            fullWidth={true}
-          />
-        </div>
-        <div>
-          <Button raised color="primary" type="submit" style={styles.submitLogin}>Login</Button>
-        </div>
-      </form>
-    </Paper>
+    <Validation username={username}>
+      <Paper style={styles.loginForm}>
+        <Typography type="headline" component="h1">Login!</Typography>
+        <form onSubmit={handleUserLogin}>
+          <div>
+            <TextField
+              type="text"
+              label="Username"
+              name="username"
+              style={styles.loginInputField}
+              fullWidth={true}
+            />
+          </div>
+          <div>
+            <TextField
+              type="password"
+              label="Password"
+              name="password"
+              style={styles.loginInputField}
+              fullWidth={true}
+            />
+          </div>
+          <div>
+            <Button raised color="primary" type="submit" style={styles.submitLogin}>Login</Button>
+          </div>
+        </form>
+      </Paper>
+    </Validation>
   )
 }
